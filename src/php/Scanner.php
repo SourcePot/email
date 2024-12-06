924827f5cc1aa060125c5309ac7f7d1058b4fb75
Carsten Wallenhauer <115737488+SourcePot@users.noreply.github.com>
2024-12-06 19:27:41 +0100

    Initial commit

---
+++ src/php/Scanner.php
@@ -0,0 +1,185 @@
+<?php
+/*
+* This file is part of the MsgToFiles package.
+* @package MsgToFiles
+* @author Carsten Wallenhauer <admin@datapool.info>
+* @copyright 2024 to today Carsten Wallenhauer
+* @license https://opensource.org/license/mit MIT
+*/
+
+declare(strict_types=1);
+
+namespace SourcePot\Email;
+
+final class Scanner{
+
+    public const DB_TIMEZONE='UTC';
+
+    private $rawMsg='';
+    private $header=array();
+    private $body=array();
+    private $msgHash='';
+    
+    function __construct(string $msg)
+    {
+        $this->rawMsg=$msg;
+        $this->msgHash=sha1($msg);
+        $this->processRawMsfg($msg);
+    }
+    
+    public function getRawMsg():string
+    {
+        return $this->rawMsg;
+    }
+
+    public function getHeader():array
+    {
+        return $this->header;
+    }
+
+    public function getBody():array
+    {
+        return $this->body;
+    }
+
+    private function processRawMsfg($msg)
+    {
+        $separatorPos=mb_strpos($msg,"\r\n\r\n");
+        if ($separatorPos===FALSE){
+            throw new \Exception("Header to content separator not found."); 
+        }
+        $msgHeader=mb_substr($msg,0,$separatorPos);
+        $this->header=$this->processHeader($msgHeader);
+        $msgBody=mb_substr($msg,$separatorPos);
+        $this->body=$this->processBody($msgBody);
+    }
+
+    private function processHeader(string $msgHeader,array $header=array()):array
+    {
+        // unfold header fields
+        $msgHeader=preg_replace('/\r\n\s+/',' ',$msgHeader);
+        // get fields
+        preg_match_all('/([^:]+):([^\r\n]+)\r\n/',$msgHeader,$fields);
+        foreach($fields[1] as $index=>$fieldName){
+            $fieldName=strtolower($fieldName);
+            $fieldBody=$fields[2][$index];
+            $fieldBodyComps=explode('||',preg_replace('/([^"])(;)([^"])/','$1||$3',$fieldBody));
+            foreach($fieldBodyComps as $fieldBodyCompIndex=>$fieldBodyComp){
+                $fieldBodyComp=trim($fieldBodyComp);
+                // mime decode
+                if (strpos($fieldBodyComp,'=?')!==FALSE && strpos($fieldBodyComp,'?=')!==FALSE){
+                    $fieldBodyComp=iconv_mime_decode($fieldBodyComp,0,"UTF-8");
+                }
+                // decode values
+                $eqSign=strpos($fieldBodyComp,'=');
+                if ($eqSign===FALSE){
+                    if (count($fieldBodyComps)>1){
+                        $header[$fieldName][$fieldBodyCompIndex]=$fieldBodyComp;
+                    } else {
+                        $header[$fieldName]=$fieldBodyComp;
+                    }
+                } else {
+                    $subKey=substr($fieldBodyComp,0,$eqSign);
+                    $subValue=substr($fieldBodyComp,$eqSign+1);
+                    $header[$fieldName][$subKey]=$subValue;
+                }
+            }
+        }
+        return $header;
+    }
+
+    private function processBody($msgBody):array
+    {
+        $body=array();
+        // scan body
+        $isHeader=FALSE;
+        $header='';
+        $content='';
+        $boundaries=array();
+        $msgBodyLines=explode("\r\n",$msgBody);
+        foreach($msgBodyLines as $lineIndex=>$line){
+            $line.="\r\n";
+            if (empty(trim($line,"\r\n"))){
+                $isHeader=FALSE;
+                continue;
+            }
+            if ($isHeader){$header.=$line;}
+            if (strpos($line,'--')===0 && strpos($line,'-->')===FALSE){
+                $isHeader=TRUE;
+                $headerArr=$this->processHeader($header,array('boundaries'=>$boundaries));
+                // update boundary
+                $boundaryA=trim($line);
+                $boundaryA=substr($boundaryA,2);
+                $bounderyB=rtrim($boundaryA,'-');
+                if ($boundaryA===$bounderyB){
+                    $boundaries[$boundaryA]=TRUE;
+                } else {
+                    $boundaries[$bounderyB]=FALSE;
+                }
+                // update body
+                $body=$this->addBodyPart($body,$headerArr,$content);
+                $content=$header='';
+            }
+            if (!$isHeader){$content.=$line;}
+        
+        }
+        return $body;
+    }
+
+    private function addBodyPart(array $body,array $headerArr,string $content):array
+    {
+        if (empty(trim($content))){return $body;}
+        if (!empty($headerArr['content-transfer-encoding'])){
+            $content=$this->decodeContent($content,$headerArr['content-transfer-encoding']);
+        }
+
+        if (!empty($headerArr['content-disposition']['filename'])){
+            $file='../tmp/'.trim($headerArr['content-disposition']['filename'],'"');
+            file_put_contents($file,$content);
+        }
+        $key=$this->keyFromHeaderArr($headerArr);
+        $body[$key]=array('header'=>$headerArr,'content'=>$content);
+        return $body;
+    }
+
+    private function decodeContent(string $content,string $encoding)
+    {
+        switch($encoding){
+            case 'base64':
+                $content=base64_decode($content);
+                break;
+            case 'quoted-printable':
+                $content=quoted_printable_decode($content);
+                break;
+            default:
+                $content=$content;
+        }
+        return $content;
+    }
+
+    private function keyFromHeaderArr(array $headerArr):string
+    {
+        $key='';
+        foreach($headerArr['boundaries'] as $boundary=>$isActive){
+            if (!empty($key)){$key.=',';}
+            $hash=md5($boundary.$this->msgHash);
+            $hash=base_convert($hash,16,32);
+            $hash=str_replace('0','',$hash);
+            $hash=str_replace('|','x',$hash);
+            $key.=$hash;
+        }
+        if (!empty($key)){
+            $key='('.$key.')';
+        }
+        if (isset($headerArr['content-type'][0])){
+            $key.='['.$headerArr['content-type'][0].']';
+        }
+        if (isset($this->header['subject'])){
+            $suffix=(mb_strlen($this->header['subject'])>40)?'... ':' ';
+            $key=mb_substr($this->header['subject'],0,40).$suffix.$key;
+        }
+        return $key;
+    }
+
+}
+?>
---
+++ src/www/index.php
@@ -0,0 +1,64 @@
+<?php
+/*
+* This file is part of the MsgToFiles package.
+* @package MsgToFiles
+* @author Carsten Wallenhauer <admin@datapool.info>
+* @copyright 2024 to today Carsten Wallenhauer
+* @license https://opensource.org/license/mit MIT
+*/
+	
+declare(strict_types=1);
+	
+namespace SourcePot\Email;
+	
+mb_internal_encoding("UTF-8");
+
+require_once('../php/Scanner.php');
+
+// form pürocessing
+$header='';
+$body='';
+if (isset($_POST['process'])){
+    if (is_file($_FILES['msg']['tmp_name'])){
+        move_uploaded_file($_FILES['msg']['tmp_name'],'../tmp/test.msg');
+        $msgContent=file_get_contents('../tmp/test.msg');
+        $msgObj=new Scanner($msgContent);
+        // present header
+        foreach($msgObj->getHeader() as $key=>$value){
+            if (is_array($value)){
+                foreach($value as $subKey=>$subValue){
+                    $header.=$key.' | '.$subKey.': '.$subValue."\n";
+                }
+            } else {
+                $header.=$key.': '.$value."\n";
+            }
+        }
+        // present body
+        foreach($msgObj->getBody() as $key=>$value){
+            $body.="\n".$key.":\n".json_encode($value['header'])."\n";
+        }
+    }
+}
+// compile html
+$html='<!DOCTYPE html>
+        <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Message processing test">
+        <meta name="robots" content="index">
+        <title>MsgToFiles</title>
+        </head>
+        <body><form name="892d183ba51083fc2a0b3d4d6453e20b" id="892d183ba51083fc2a0b3d4d6453e20b" method="post" enctype="multipart/form-data">';
+$html.='<h1 style="float:left;clear:both;margin:1em 0em 0.5em">Email Scanner Test</h1>';
+$html.='<div style="float:left;clear:both;padding:0.25em 1em;border:1px solid #000;"><label for="msg-file-upload">Upload your test message file</label><input type="file" name="msg" id="msg-file-upload" style="margin:0.25em;"/><input type="submit" name="process" id="msg-file-process" style="margin:0.25em;" value="Process"/></div>';
+$html.='</form>';
+$html.='<p style="float:left;clear:both;display:block;white-space:pre;word-break:break-all;word-wrap:anywhere;width:95vw;"><br/>';
+$html.='============================= HEADER ==========================<br/>';
+$html.=htmlentities($header).'<br/>';
+$html.='=========================== BODY PARTS ==========================<br/>';
+$html.=htmlentities($body).'</p>';
+$html.='</body></html>';
+
+echo $html;
+?>
